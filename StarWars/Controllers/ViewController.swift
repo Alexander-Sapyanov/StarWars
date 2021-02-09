7ae6be7639fe2b8b9601ae12e0bceaa6bd16431f
Alexander Sapyanov <work.sapyanov@gmail.com>
2021-02-09 13:16:02 +0400

    09.02.2021

--- StarWars/Controllers/ViewController.swift
+++ StarWars/Controllers/ViewController.swift
@@ -10,8 +10,11 @@ import Alamofire
 
 class ViewController: UIViewController {
     
-    var filmsTableView: UITableView!
+    
+    
+    var filmsCollectionView: UICollectionView!
     var films: [Film] = []
+    let imageForFilm = ["A New Hope","The Empire strikes back","Return of the Jedi","Phantom menace","Attack of the clones","Revenge of the sith"]
   
 
     // MARK: - Lifecycles
@@ -27,35 +30,47 @@ class ViewController: UIViewController {
             .validate().responseDecodable(of: Films.self) { (response) in
                 guard let films = response.value else { return }
                 self.films = films.all
-                self.filmsTableView.reloadData()
+                self.filmsCollectionView.reloadData()
                 
                 print(films.count)
             }
     }
     
     func createTableView() {
-        filmsTableView = UITableView()
-        filmsTableView.dataSource = self
-        filmsTableView.frame = view.frame
-        filmsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        view.addSubview(filmsTableView!)
+        let frame = CGRect(x:0 , y: Int(view.bounds.size.height / 2 ), width: Int(view.bounds.size.width) , height: Int(view.bounds.size.height / 2)  )
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        filmsCollectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
+        filmsCollectionView.dataSource = self
+        filmsCollectionView.delegate = self
+        filmsCollectionView.backgroundColor = .white
+        
+        
+        filmsCollectionView.register(FilmCollectionViewCell.self, forCellWithReuseIdentifier: FilmCollectionViewCell.identifire)
+        view.addSubview(filmsCollectionView)
     }
 }
 
     // MARK: - Extensions
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return films.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
-        let item = films[indexPath.row]
-        cell.backgroundColor = .red
-        cell.textLabel?.text = item.title
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FilmCollectionViewCell.identifire,for: indexPath) as! FilmCollectionViewCell
+     
+        cell.nameLabel.text = films[indexPath.row].title
+        cell.caseImageView.image = UIImage(named: imageForFilm[indexPath.row])
+        cell.layer.cornerRadius = 18
         return cell
     }
-    
-    
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 300)
+    }
+}
---
+++ StarWars/View/FilmCollectionViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  FilmTableViewCell.swift
+//  StarWars
+//
+//  Created by Alexander  Sapianov on 08.02.2021.
+//
+
+import UIKit
+
+class FilmCollectionViewCell: UICollectionViewCell {
+    
+    static let identifire = "Cell"
+
+    
+    override init(frame: CGRect) {
+         super.init(frame: frame)
+
+         addViews()
+     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 22)
+        label.text = ""
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var caseImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .white
+        imageView.image = UIImage(named: "")
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 18
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    func addViews() {
+        addSubview(caseImageView)
+        addSubview(nameLabel)
+        
+        
+        
+        nameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 5).isActive = true
+        nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor,constant: -10).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        nameLabel.widthAnchor.constraint(equalToConstant: contentView.bounds.size.width).isActive = true
+        
+        caseImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 5).isActive = true
+        caseImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -5).isActive = true
+        caseImageView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        caseImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50).isActive = true
+    }
+}
